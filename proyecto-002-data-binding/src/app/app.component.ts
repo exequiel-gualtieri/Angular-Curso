6beb2d9b3d40909bc4a308d61d831643098b2d43
Exequiel <exequielgualtieri.dev@gmail.com>
2023-01-22 23:19:39 +0100

    Validación de código al agregar un nuevo artículo

--- proyecto-002-data-binding/src/app/app.component.ts
+++ proyecto-002-data-binding/src/app/app.component.ts
@@ -13,17 +13,37 @@ export class AppComponent {
   precio:number = 0;
 
   agregar() {
-    if(!this.codigo || !this.descripcion || !this.precio) {
+    if( !this.descripcion || !this.precio) {
       alert('Todos los campos son obligatorios')
       return;
     }
-    let articulo: Articulo = new Articulo(this.codigo, this.descripcion, this.precio);
+    
+
+    let articulo: Articulo = new Articulo(this.articulos.length, this.descripcion, this.precio);
     this.articulos.push(articulo);
-
+    this.ordenar
     alert('El artículo se agregó de forma correcta');
+    
+    this.codigo = 0;
+    this.descripcion = '';
+    this.precio = 0;
+
   }
 
-  eliminar() {
-
+  borrar(codigo: number) {
+    const resultado = confirm('¿Desea eliminar este artículo?')
+    if(resultado) this.articulos = this.articulos.filter(e => e.codigo !== codigo);
+    this.ordenar()
   }
+
+  buscarArticulo(codigo:number) {
+    return this.articulos.find(e => e.codigo === codigo)
+  }
+
+  ordenar() {
+    this.articulos.forEach((e,i)=> {
+      e.codigo = i + 1
+    })
+  }
+  // eliminar(art) {
 }
