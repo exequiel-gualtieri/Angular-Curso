d4522002510b2f6532d2d3ea1c85948c79fe7733
Exequiel <exequielgualtieri.dev@gmail.com>
2023-01-22 22:25:18 +0100

    Data binding para el formulario - Modelo para artículos

--- proyecto-002-data-binding/src/app/app.component.ts
+++ proyecto-002-data-binding/src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  artículos = [];
+  artículos:[] = [];
+  codigo:number = 0;
+  descripcion:string = '';
+  precio:number = 0;
+
+  agregar() {
+    if(this.codigo || this.descripcion || this.precio) {
+      alert('Todos los campos son obligatorios')
+      return;
+    }
+
+    
+  }
 }
---
+++ proyecto-002-data-binding/src/app/models/articulos.model.ts
@@ -0,0 +1,18 @@
+class Articulo {
+
+  codigo: number;
+  descripción: string;
+  precio: number;  
+  
+  constructor(
+    codigo:number,
+    descripcion:string,
+    precio:number
+    ) {
+      
+      this.codigo = codigo;
+      this.descripción = descripcion;
+      this.precio = precio;
+
+  }
+}
